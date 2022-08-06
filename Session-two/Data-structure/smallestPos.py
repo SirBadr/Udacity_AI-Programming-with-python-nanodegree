accadfc53466f7f9ae7e3df7491943c5a42f7f81
SirBadr <tso1999@yahoo.com>
2022-08-07 00:32:55 +0200

    remove unwanted file

--- Session-two/Data-structure/smallestPos.py
+++
@@ -1,9 +0,0 @@
-def smallest_positive(in_list):
-    # TODO: Define a control structure that finds the smallest positive
-    # number in in_list and returns the correct smallest number.
-    return 0
-# Test cases
-print(smallest_positive([4, -6, 7, 2, -4, 10]))
-# Correct output: 2
-print(smallest_positive([.2, 5, 3, -.1, 7, 7, 6]))
-# Correct output: 0.2
