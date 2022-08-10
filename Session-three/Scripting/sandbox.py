476908c12c30b57ddbe50d286d1c7619ff0c8b32
SirBadr <tso1999@yahoo.com>
2022-08-10 10:33:43 +0200

    Session-three: import python scripts

---
+++ Session-three/Scripting/sandbox.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.insert(0, '../')
+sys.path.insert(0, '../../Session-one')
+from sandbox2 import tfunction as tff
+from twosum import twoSumTwo as tst
+
+print(tff(2))
+print(tst([1,2,3,5], 3))
+#print(__name__)
+#print(sb2.__name__)
